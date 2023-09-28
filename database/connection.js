86ba4394c6ddcf62d10995a90f889f0de9faf581
jnrpalma <nicolas.jnr10@outlook.com>
2023-09-28 11:41:45 -0300

    ajusta banco de dados acesso

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ const dbPassword = process.env.DB_PASS;
 
 
 const connect = () => {
-    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.hzazbvi.mongodb.net/teste?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.jl2nufa.mongodb.net/test?retryWrites=true&w=majority`)
 
     const connection = mongoose.connection;
 
